49de0242e36d87139e7322effbd65ff7aa5e69b1
Ivan Skliaruk <cunanori@gmail.com>
2024-01-18 12:45:18 +0100

    hw 4 upd

--- src/video.py
+++ src/video.py
@@ -18,10 +18,11 @@ class Video:
     def __str__(self):
         return self.video_title
 
-class PLVideo:
+class PLVideo(Video):
+
     def __init__(self, video_id, playlist_id):
         youtube = build('youtube', 'v3', developerKey=api_key)
-        self.video = youtube.videos().list(id=video_id, part='snippet,statistics,contentDetails').execute()
+        super().__init__(video_id)
         self.playlist_videos = youtube.playlistItems().list(playlistId=playlist_id,
                                                        part='contentDetails',
                                                        maxResults=50,
@@ -31,6 +32,3 @@ class PLVideo:
         self.view_count = self.video['items'][0]['statistics']['viewCount']
         self.like_count = self.video['items'][0]['statistics']['likeCount']
         self.playlist_id = playlist_id
-
-    def __str__(self):
-        return self.video_title
